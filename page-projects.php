5ac4e1d68f27b7f168b2a8de913da1996fe01243
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-12-03 16:18:55 +0000

    Initial styles for project page

---
+++ page-projects.php
@@ -0,0 +1,72 @@
+<?php get_header(); ?>
+<div class="jc_wrap">
+    <div class="jc_page-hero">
+        Hero Image
+    </div>
+        <ul class="jc_news-archive jc_square-grid">
+                <li>
+                    <div class="jc_news-archive-item">
+                        <a href="#" title="Read">
+                            <img src="<?php bloginfo('stylesheet_directory'); ?>/img/square.jpg" alt="">
+                            <div class="jc_news-archive-overlay">
+                                <h2>Project Title</h2>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="jc_news-archive-item">
+                        <a href="#" title="Read">
+                            <img src="<?php bloginfo('stylesheet_directory'); ?>/img/square.jpg" alt="">
+                            <div class="jc_news-archive-overlay">
+                                <h2>Project Title</h2>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="jc_news-archive-item">
+                        <a href="#" title="Read">
+                            <img src="<?php bloginfo('stylesheet_directory'); ?>/img/square.jpg" alt="">
+                            <div class="jc_news-archive-overlay">
+                                <h2>Project Title</h2>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="jc_news-archive-item">
+                        <a href="#" title="Read">
+                            <img src="<?php bloginfo('stylesheet_directory'); ?>/img/square.jpg" alt="">
+                            <div class="jc_news-archive-overlay">
+                                <h2>Project Title</h2>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="jc_news-archive-item">
+                        <a href="#" title="Read">
+                            <img src="<?php bloginfo('stylesheet_directory'); ?>/img/square.jpg" alt="">
+                            <div class="jc_news-archive-overlay">
+                                <h2>Project Title</h2>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+                <li>
+                    <div class="jc_news-archive-item">
+                        <a href="#" title="Read">
+                            <img src="<?php bloginfo('stylesheet_directory'); ?>/img/square.jpg" alt="">
+                            <div class="jc_news-archive-overlay">
+                                <h2>Project Title</h2>
+                            </div>
+                        </a>
+                    </div>
+                </li>
+        </ul>
+        <nav class="jc_pagination">
+
+        </nav>
+</div>
+<?php get_footer(); ?>
