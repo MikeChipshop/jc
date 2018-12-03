fb6d34673653fbada41cf23dcab514e0468f1ac6
Mike Bishop <mike@miniman-webdesign.co.uk>
2018-12-03 15:26:14 +0000

    Added main menu

--- header.php
+++ header.php
@@ -17,11 +17,7 @@
         </div>
         <nav>
             <ul>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Practice</a></li>
-                <li><a href="#">News</a></li>
-                <li><a href="#">Capital</a></li>
-                <li><a href="#">Contact</a></li>
+                <?php wp_nav_menu( array('theme_location' => 'main_menu' )); ?>
                 <li><a href="#"><i class="fal fa-search fa-flip-horizontal"></i></a></li>
             </ul>
         </nav>
