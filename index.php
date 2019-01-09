4b41a282794a98f564aa09e5eb2787f07a9fc68c
MikeChipshop <mike@miniman-webdesign.co.uk>
2019-01-09 09:53:04 +0000

    Index news page excerpts

--- index.php
+++ index.php
@@ -13,8 +13,7 @@
                             <img src="<?php the_post_thumbnail_url('news'); ?> " alt="<?php the_title(); ?>">
                             <div class="jc_news-archive-overlay">
                                 <h2><?php the_title(); ?></h2>
-                                <p>Bio Acit latur alique iunt aut est, que listis quam solupta nostium sit pa ducit, tet qui to doluptam idebit quos maximol orrunt.</p>
-                                <p>Neceatqui omnis dolupti busdam aceprovid qui sim volendandita sum et occab incti quiam sitata parum que et pror sitem</p>
+                                <?php the_excerpt(); ?>
                                 <div class="jc_news-archive-icon"><i class="fal fa-arrow-down"></i></div>
                             </div>
 
