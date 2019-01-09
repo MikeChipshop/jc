2a5244e7b59b9d4901890ea7d21a9bc07aa742f3
MikeChipshop <mike@miniman-webdesign.co.uk>
2019-01-09 10:54:10 +0000

    Video selection for front page boxes

--- front-page.php
+++ front-page.php
@@ -11,8 +11,12 @@
                         $image = wp_get_attachment_image_src( $attachment_id, $size );
                     ?>
                     <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_1_title'); ?>">
-                    <a href="<?php the_field('block_1_link'); ?>" class="jc_overlay">
-                        <h2><?php the_field('block_1_title'); ?></h2>
+                    <a href="<?php the_field('block_1_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_1_video_embed_code'); ?>">
+                        <?php if(get_field('block_1_video')): ?>
+                            <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                        <?php else: ?>
+                            <h2><?php the_field('block_1_title'); ?></h2>
+                        <?php endif; ?>
                     </a>
                 </div>
             </div>
@@ -109,28 +113,36 @@
                 </div>
             </div>
             <div class="jc_grid-50">
-                <div class="cat-d jc_grid-flex-content" id="block-3">
+                <div class="cat-d jc_grid-flex-content<?php if(get_field('block_3_video')): ?> jc_video-box<?php endif; ?>" id="block-3">
                     <?php
                         $attachment_id = get_field('block_3_image');
                         $size = "full";
                         $image = wp_get_attachment_image_src( $attachment_id, $size );
                     ?>
                     <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_3_title'); ?>">
-                    <a href="<?php the_field('block_3_link'); ?>" class="jc_overlay">
-                        <h2><?php the_field('block_3_title'); ?></h2>
+                    <a href="<?php the_field('block_3_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_3_video_embed_code'); ?>">
+                        <?php if(get_field('block_3_video')): ?>
+                            <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                        <?php else: ?>
+                            <h2><?php the_field('block_3_title'); ?></h2>
+                        <?php endif; ?>
                     </a>
                 </div>
             </div>
             <div class="jc_grid-100">
-                <div class="cat-b jc_grid-flex-content" id="block-6">
+                <div class="cat-b jc_grid-flex-content<?php if(get_field('block_6_video')): ?> jc_video-box<?php endif; ?>" id="block-6">
                     <?php
                         $attachment_id = get_field('block_6_image');
                         $size = "full";
                         $image = wp_get_attachment_image_src( $attachment_id, $size );
                     ?>
                     <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_6_title'); ?>">
-                    <a href="<?php the_field('block_6_link'); ?>" class="jc_overlay">
-                        <h2><?php the_field('block_6_title'); ?></h2>
+                    <a href="<?php the_field('block_6_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_6_video_embed_code'); ?>">
+                        <?php if(get_field('block_6_video')): ?>
+                            <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                        <?php else: ?>
+                            <h2><?php the_field('block_6_title'); ?></h2>
+                        <?php endif; ?>
                     </a>
                 </div>
             </div>
@@ -140,28 +152,36 @@
 <div class="jc_wrap jc_front-page-grid">
     <div class="jc_grid-100 jc_grid-flex">
         <div class="jc_grid-33">
-            <div class="cat-a jc_grid-flex-content" id="block-7">
+            <div class="cat-a jc_grid-flex-content<?php if(get_field('block_7_video')): ?> jc_video-box<?php endif; ?>" id="block-7">
                 <?php
                     $attachment_id = get_field('block_7_image');
                     $size = "full";
                     $image = wp_get_attachment_image_src( $attachment_id, $size );
                 ?>
                 <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_7_title'); ?>">
-                <a href="<?php the_field('block_7_link'); ?>" class="jc_overlay">
-                    <h2><?php the_field('block_7_title'); ?></h2>
+                <a href="<?php the_field('block_7_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_7_video_embed_code'); ?>">
+                    <?php if(get_field('block_7_video')): ?>
+                        <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                    <?php else: ?>
+                        <h2><?php the_field('block_7_title'); ?></h2>
+                    <?php endif; ?>
                 </a>
             </div>
         </div>
         <div class="jc_grid-33">
-            <div class="cat-d jc_grid-flex-content" id="block-8">
+            <div class="cat-d jc_grid-flex-content<?php if(get_field('block_8_video')): ?> jc_video-box<?php endif; ?>" id="block-8">
                 <?php
                     $attachment_id = get_field('block_8_image');
                     $size = "full";
                     $image = wp_get_attachment_image_src( $attachment_id, $size );
                 ?>
                 <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_8_title'); ?>">
-                <a href="<?php the_field('block_8_link'); ?>" class="jc_overlay">
-                    <h2><?php the_field('block_8_title'); ?></h2>
+                <a href="<?php the_field('block_8_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_8_video_embed_code'); ?>">
+                    <?php if(get_field('block_8_video')): ?>
+                        <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                    <?php else: ?>
+                        <h2><?php the_field('block_8_title'); ?></h2>
+                    <?php endif; ?>
                 </a>
             </div>
         </div>
@@ -201,28 +221,36 @@
                 </div>
             </div>
             <div class="jc_grid-100 jc_grid-flex">
-                <div class="cat-c jc_grid-flex-content" id="block-10">
+                <div class="cat-c jc_grid-flex-content<?php if(get_field('block_10_video')): ?> jc_video-box<?php endif; ?>" id="block-10">
                     <?php
                         $attachment_id = get_field('block_10_image');
                         $size = "full";
                         $image = wp_get_attachment_image_src( $attachment_id, $size );
                     ?>
                     <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_10_title'); ?>">
-                    <a href="<?php the_field('block_10_link'); ?>" class="jc_overlay">
-                        <h2><?php the_field('block_10_title'); ?></h2>
+                    <a href="<?php the_field('block_10_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_10_video_embed_code'); ?>">
+                        <?php if(get_field('block_10_video')): ?>
+                            <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                        <?php else: ?>
+                            <h2><?php the_field('block_10_title'); ?></h2>
+                        <?php endif; ?>
                     </a>
                 </div>
             </div>
             <div class="jc_grid-100 jc_grid-flex">
-                <div class="cat-b jc_grid-flex-content" id="block-11">
+                <div class="cat-b jc_grid-flex-content<?php if(get_field('block_11_video')): ?> jc_video-box<?php endif; ?>" id="block-11">
                     <?php
                         $attachment_id = get_field('block_11_image');
                         $size = "full";
                         $image = wp_get_attachment_image_src( $attachment_id, $size );
                     ?>
                     <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_11_title'); ?>">
-                    <a href="<?php the_field('block_11_link'); ?>" class="jc_overlay">
-                        <h2><?php the_field('block_11_title'); ?></h2>
+                    <a href="<?php the_field('block_11_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_11_video_embed_code'); ?>">
+                        <?php if(get_field('block_11_video')): ?>
+                            <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                        <?php else: ?>
+                            <h2><?php the_field('block_11_title'); ?></h2>
+                        <?php endif; ?>
                     </a>
                 </div>
             </div>
@@ -231,28 +259,36 @@
 
     <div class="jc_grid-100 jc_grid-flex">
         <div class="jc_grid-66">
-            <div class="cat-b jc_grid-flex-content" id="block-12">
+            <div class="cat-b jc_grid-flex-content<?php if(get_field('block_12_video')): ?> jc_video-box<?php endif; ?>" id="block-12">
                 <?php
                         $attachment_id = get_field('block_12_image');
                         $size = "full";
                         $image = wp_get_attachment_image_src( $attachment_id, $size );
                     ?>
                     <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_12_title'); ?>">
-                <a href="<?php the_field('block_12_link'); ?>" class="jc_overlay">
-                    <h2><?php the_field('block_12_title'); ?></h2>
+                <a href="<?php the_field('block_12_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_12_video_embed_code'); ?>">
+                    <?php if(get_field('block_12_video')): ?>
+                        <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                    <?php else: ?>
+                        <h2><?php the_field('block_12_title'); ?></h2>
+                    <?php endif; ?>
                 </a>
             </div>
         </div>
         <div class="jc_grid-33">
-            <div class="cat-a jc_grid-flex-content" id="block-13">
+            <div class="cat-a jc_grid-flex-content<?php if(get_field('block_13_video')): ?> jc_video-box<?php endif; ?>" id="block-13">
                 <?php
                         $attachment_id = get_field('block_13_image');
                         $size = "full";
                         $image = wp_get_attachment_image_src( $attachment_id, $size );
                     ?>
                     <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_13_title'); ?>">
-                <a href="<?php the_field('block_13_link'); ?>" class="jc_overlay">
-                    <h2><?php the_field('block_13_title'); ?></h2>
+                <a href="<?php the_field('block_13_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_13_video_embed_code'); ?>">
+                    <?php if(get_field('block_13_video')): ?>
+                        <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                    <?php else: ?>
+                        <h2><?php the_field('block_13_title'); ?></h2>
+                    <?php endif; ?>
                 </a>
             </div>
         </div>
@@ -260,28 +296,36 @@
 
     <div class="jc_grid-100 jc_grid-flex">
         <div class="jc_grid-33">
-            <div class="cat-c jc_grid-flex-content" id="block-14">
+            <div class="cat-c jc_grid-flex-content<?php if(get_field('block_14_video')): ?> jc_video-box<?php endif; ?>" id="block-14">
                 <?php
                         $attachment_id = get_field('block_14_image');
                         $size = "full";
                         $image = wp_get_attachment_image_src( $attachment_id, $size );
                     ?>
                     <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_14_title'); ?>">
-                <a href="<?php the_field('block_14_link'); ?>" class="jc_overlay">
-                    <h2><?php the_field('block_14_title'); ?></h2>
+                <a href="<?php the_field('block_14_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_14_video_embed_code'); ?>">
+                    <?php if(get_field('block_14_video')): ?>
+                        <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                    <?php else: ?>
+                        <h2><?php the_field('block_14_title'); ?></h2>
+                    <?php endif; ?>
                 </a>
             </div>
         </div>
         <div class="jc_grid-33">
-            <div class="cat-d jc_grid-flex-content" id="block-15">
+            <div class="cat-d jc_grid-flex-content<?php if(get_field('block_15_video')): ?> jc_video-box<?php endif; ?>" id="block-15">
                 <?php
                     $attachment_id = get_field('block_15_image');
                     $size = "full";
                     $image = wp_get_attachment_image_src( $attachment_id, $size );
                 ?>
                 <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_15_title'); ?>">
-                <a href="<?php the_field('block_15_link'); ?>" class="jc_overlay">
-                    <h2><?php the_field('block_15_title'); ?></h2>
+                <a href="<?php the_field('block_15_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_15_video_embed_code'); ?>">
+                    <?php if(get_field('block_15_video')): ?>
+                        <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                    <?php else: ?>
+                        <h2><?php the_field('block_15_title'); ?></h2>
+                    <?php endif; ?>
                 </a>
             </div>
         </div>
@@ -363,28 +407,36 @@
 
     <div class="jc_grid-100 jc_grid-flex">
         <div class="jc_grid-33">
-            <div class="cat-a jc_grid-flex-content" id="block-18">
+            <div class="cat-a jc_grid-flex-content<?php if(get_field('block_18_video')): ?> jc_video-box<?php endif; ?>" id="block-18">
                 <?php
                     $attachment_id = get_field('block_18_image');
                     $size = "full";
                     $image = wp_get_attachment_image_src( $attachment_id, $size );
                 ?>
                 <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_18_title'); ?>">
-                <a href="<?php the_field('block_18_link'); ?>" class="jc_overlay">
-                    <h2><?php the_field('block_18_title'); ?></h2>
+                <a href="<?php the_field('block_18_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_18_video_embed_code'); ?>">
+                    <?php if(get_field('block_18_video')): ?>
+                        <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                    <?php else: ?>
+                        <h2><?php the_field('block_18_title'); ?></h2>
+                    <?php endif; ?>
                 </a>
             </div>
         </div>
         <div class="jc_grid-66">
-            <div class="cat-b jc_grid-flex-content" id="block-19">
+            <div class="cat-b jc_grid-flex-content<?php if(get_field('block_19_video')): ?> jc_video-box<?php endif; ?>" id="block-19">
                 <?php
                     $attachment_id = get_field('block_19_image');
                     $size = "full";
                     $image = wp_get_attachment_image_src( $attachment_id, $size );
                 ?>
                 <img src="<?php echo $image[0]; ?>" alt="Visit <?php the_field('block_19_title'); ?>">
-                <a href="<?php the_field('block_19_link'); ?>" class="jc_overlay">
-                    <h2><?php the_field('block_19_title'); ?></h2>
+                <a href="<?php the_field('block_19_link'); ?>" class="jc_overlay" data-embed="https://player.vimeo.com/video/<?php the_field('block_19_video_embed_code'); ?>">
+                    <?php if(get_field('block_19_video')): ?>
+                        <h2 class="jc_play-button"><i class="fas fa-play"></i></h2>
+                    <?php else: ?>
+                        <h2><?php the_field('block_19_title'); ?></h2>
+                    <?php endif; ?>
                 </a>
             </div>
         </div>
--- taxonomy-project-category.php
+++ taxonomy-project-category.php
@@ -6,11 +6,11 @@
                     <li>
                         <div class="jc_news-archive-item">
                             <?php
-                                $attachment_id = get_field('taxonomy_image',$term->term_id);
+                                $attachment_id = get_field('taxonomy_image');
                                 $size = "square";
                                 $image = wp_get_attachment_image_src( $attachment_id, $size );
                             ?>
-                            <a href="">
+                            <a href="<?php the_permalink(); ?>">
                                 <img src="<?php echo $image[0]; ?>" alt="">
                                 <div class="jc_news-archive-overlay">
                                     <h2><?php the_title(); ?></h2>
